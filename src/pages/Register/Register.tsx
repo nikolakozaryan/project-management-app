8d7e40d2bb34860928cf1096de490d3ca6b6fa9a
Nikolay <kolyak770@gmail.com>
2022-11-17 01:50:30 +0300

    feat: fill Register page with components

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import LoginInput from '../../components/Register/LoginInput/LoginInput';
+import NameInput from '../../components/Register/NameInput/NameInput';
+import PasswordInput from '../../components/Register/PasswordInput/PasswordInput';
+import { FormInputs } from './types';
+import classes from './Register.module.scss';
+import Button from '../../components/common/Button/Button';
 
 const Register = () => {
-  return <div>Register</div>;
+  const methods = useForm<FormInputs>({ reValidateMode: 'onSubmit' });
+
+  const formSubmitHandler: SubmitHandler<FormInputs> = (data, event) => {
+    event?.preventDefault();
+  };
+
+  return (
+    <section className={classes.register}>
+      <h2 className={classes.register__heading}>Регистрация</h2>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(formSubmitHandler)} className={classes.register__form}>
+          <NameInput />
+          <LoginInput />
+          <PasswordInput />
+          <Button value="зарегистрироваться" />
+        </form>
+      </FormProvider>
+    </section>
+  );
 };
 
 export default Register;
