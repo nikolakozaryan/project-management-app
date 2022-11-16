932a0dcf48b4d3e49675f739cf6ad21be30d1f80
nikolakozaryan <48277561+nikolakozaryan@users.noreply.github.com>
2022-11-16 12:04:53 +0300

    #4 Create router (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Router from './components/common/Router/Router';
 
-const App = () => <div className="App"></div>;
+const App = () => (
+  <div className="App">
+    <Router />
+  </div>
+);
 
 export default App;
---
+++ src/components/common/Router/Router.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../../../pages/Dashboard/Dashboard';
+import Home from '../../../pages/Home/Home';
+import Login from '../../../pages/Login/Login';
+import NotFound from '../../../pages/NotFound/NotFound';
+import Register from '../../../pages/Register/Register';
+
+const Router = () => (
+  <Routes>
+    <Route index element={<Home />} />
+    <Route path="home" element={<Home />} />
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="signin" element={<Login />} />
+    <Route path="signup" element={<Register />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
+
+export default Router;
