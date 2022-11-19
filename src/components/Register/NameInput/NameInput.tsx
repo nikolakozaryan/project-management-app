baaa05ba03690d8d06057ddafb9f61efdea7c9d7
Nikolay <kolyak770@gmail.com>
2022-11-19 17:28:51 +0300

    feat: add name input logic

--- src/components/Register/NameInput/NameInput.tsx
+++ src/components/Register/NameInput/NameInput.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import { useInput } from '../../../hooks/useInput';
 import classes from './NameInput.module.scss';
 
 const NameInput = () => {
-  const methods = useFormContext();
+  const { methods, invalid, error } = useInput('name');
 
   return (
-    <input {...methods.register('name')} placeholder="Имя" className={classes.input} type="text" />
+    <div className={classes.input__wrapper}>
+      <input
+        {...methods.register('name', {
+          minLength: { value: 4, message: 'быть не короче 4 символов' },
+          maxLength: { value: 20, message: 'быть не длинее 20 символов' },
+          validate: {
+            letters: (v) => /^[A-ZА-Я]/.test(v) || 'начинаться с большой буквы',
+            start: (v) => /^.[a-zа-я]+$/.test(v) || 'содержать только буквы',
+          },
+        })}
+        placeholder="Имя"
+        className={`${classes.input} ${invalid ? classes.input_invalid : ''}`}
+        type="text"
+      />
+      {error && invalid ? <p className={classes.error}>Имя должно {error}</p> : null}
+    </div>
   );
 };
 
