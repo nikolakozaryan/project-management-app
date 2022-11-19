01058f34475577ce32103cda85617f4fdfba2080
Nikolay <kolyak770@gmail.com>
2022-11-19 17:27:44 +0300

    feat: add login input logic

--- src/components/Register/LoginInput/LoginInput.tsx
+++ src/components/Register/LoginInput/LoginInput.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import { useInput } from '../../../hooks/useInput';
 import classes from './LoginInput.module.scss';
 
 const LoginInput = () => {
-  const methods = useFormContext();
+  const { methods, invalid, error } = useInput('login');
 
   return (
-    <input
-      {...methods.register('login')}
-      placeholder="Логин"
-      className={classes.input}
-      type="text"
-    />
+    <div className={classes.input__wrapper}>
+      <input
+        {...methods.register('login', {
+          minLength: { value: 4, message: 'быть не короче 4 символов' },
+          maxLength: { value: 16, message: 'быть не длинее 10 символов' },
+          validate: {
+            letters: (v) => /(?=.*([a-z]|[A-Z]|[0-9]))/.test(v) || 'содержать латинские буквы',
+            start: (v) => /(^([a-z]|[A-Z]))/.test(v) || 'начинаться с латинской буквы',
+          },
+        })}
+        placeholder="Логин"
+        className={`${classes.input} ${invalid ? classes.input_invalid : ''}`}
+        type="text"
+      />
+      {error && invalid ? <p className={classes.error}>Логин должен {error}</p> : null}
+    </div>
   );
 };
 
