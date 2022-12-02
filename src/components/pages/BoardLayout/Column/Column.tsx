fe81f5b180f1846b6a9d371b6e963d26d2da52ad
YaGrig <yashagrigorev@gmail.com>
2022-12-02 19:16:45 +0300

    feat: add dnd to project

--- src/components/pages/BoardLayout/Column/Column.tsx
+++ src/components/pages/BoardLayout/Column/Column.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import ModalDelete from '../../../common/modalDelete/modalDelete';
 import classes from './Column.module.scss';
 
 type MyProps = {
   title: string;
   columnId: string;
+  index: number;
 };
 
-const Column: FC<MyProps> = ({ title, columnId }) => {
+const Column: FC<MyProps> = ({ title, columnId, index }) => {
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const handleDeleteClick = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
@@ -16,15 +18,24 @@ const Column: FC<MyProps> = ({ title, columnId }) => {
   };
 
   return (
-    <div className={classes.column}>
-      {showDeleteModal ? (
-        <ModalDelete type="deleteColumn" id={columnId} setModal={setShowDeleteModal} />
-      ) : null}
-      <span onClick={handleDeleteClick} className={classes.column__delete} />
-      <h5 className={classes.column__heading} title={title}>
-        {title}
-      </h5>
-    </div>
+    <Draggable draggableId={columnId} index={index}>
+      {(provided) => (
+        <div
+          className={classes.column}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          {showDeleteModal ? (
+            <ModalDelete type="deleteColumn" id={columnId} setModal={setShowDeleteModal} />
+          ) : null}
+          <span onClick={handleDeleteClick} className={classes.column__delete} />
+          <h5 className={classes.column__heading} title={title}>
+            {title}
+          </h5>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
--- src/components/pages/BoardLayout/CurrentBoard/CurrentBoard.tsx
+++ src/components/pages/BoardLayout/CurrentBoard/CurrentBoard.tsx
@@ -6,28 +6,68 @@ import { getColumns } from '../../../../features/board/boardSlice';
 import NewItem from '../../../common/NewItem/NewItem';
 import ModalDesk from '../../DeskLayout/ModalDesk/ModalDesk';
 import Column from '../Column/Column';
+import { Droppable, DropResult, DragDropContext } from 'react-beautiful-dnd';
 
 const CurrentBoard: FC<MyProps> = ({ id }) => {
   const dispatch = useAppDispatch();
   const columns = useAppSelector((state) => state.board.columns);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [allColumns, setAllColumns] = useState(columns);
+
+  console.log(allColumns, 'a;;', columns);
 
   useEffect(() => {
     dispatch(getColumns(id));
   }, []);
 
+  useEffect(() => {
+    setAllColumns(columns);
+  }, [columns]);
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+    const newBoardIds = allColumns.map((item) => item._id);
+    newBoardIds.splice(source.index, 1);
+    newBoardIds.splice(destination.index, 0, draggableId);
+
+    console.log(newBoardIds);
+  };
+
   return (
-    <div className={classes.board}>
-      {showAddModal ? <ModalDesk type="newColumn" id={id} setModal={setShowAddModal} /> : null}
-      <div className={classes.columns}>
-        <div className={classes.columns__container}>
-          {columns.map((column) => (
-            <Column key={column._id} title={column.title} columnId={column._id} />
-          ))}
-          <NewItem setIsModalAdd={setShowAddModal} type="column" />
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className={classes.board}>
+        {showAddModal ? <ModalDesk type="newColumn" id={id} setModal={setShowAddModal} /> : null}
+        <div className={classes.columns}>
+          <Droppable droppableId={id}>
+            {(provided) => (
+              <div
+                className={classes.columns__container}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {columns.map((column, index) => (
+                  <Column
+                    key={column._id}
+                    title={column.title}
+                    columnId={column._id}
+                    index={index}
+                  />
+                ))}
+
+                {provided.placeholder}
+                <NewItem setIsModalAdd={setShowAddModal} type="column" />
+              </div>
+            )}
+          </Droppable>
         </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 };
 
