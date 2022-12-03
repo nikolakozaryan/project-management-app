f217c1fbcfbd892144064fe7ab939605355c28ae
Nikolay <kolyak770@gmail.com>
2022-12-03 14:50:17 +0300

    feat: add column styles

--- src/components/pages/BoardLayout/Column/TasksContainer/TasksContainer.tsx
+++ src/components/pages/BoardLayout/Column/TasksContainer/TasksContainer.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
+import classes from './TasksContainer.module.scss';
 
 type MyProps = { children: JSX.Element };
 
 const TasksContainer: FC<MyProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={classes.tasks__container}>
+      <div className={classes.tasks}>{children}</div>
+    </div>
+  );
 };
 
 export default TasksContainer;
