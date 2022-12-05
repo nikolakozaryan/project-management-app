956c61798653a9d0da68e56b221d913310536cd8
Nikolay <kolyak770@gmail.com>
2022-12-06 00:12:25 +0300

    fix: fix styles and constants

--- src/constants/TeamMembers.ts
+++ src/constants/TeamMembers.ts
@@ -14,8 +14,8 @@ export const TEAM_MEMBERS: TeamMember[] = [
   {
     name: { ru: 'Козырев Николай', en: 'Nikolay Kozyrev' },
     done: {
-      ru: 'Пока ничего...',
-      en: 'Did nothing yet...',
+      ru: 'Структура приложения, взаимодействие с бэк-эндом, регистрация, авторизация и редактирование пользователей, страница доски, хранилище Redux, адаптивный дизайн приложения.',
+      en: 'App structure, back-end interactions, registration, authorization and editing users, board page, Redux store, adaptive app design.',
     },
     image: 'NZ.jpg',
     link: 'https://github.com/nikolakozaryan',
@@ -26,8 +26,8 @@ export const TEAM_MEMBERS: TeamMember[] = [
       en: 'Yakov Grigorev',
     },
     done: {
-      ru: 'Пока ничего...',
-      en: 'Did nothing yet...',
+      ru: 'Домашняя страница, страница досок, drag-and-drop, взаимодействие с бэк-эндом, модальные окна, видео-гайд.',
+      en: 'Home page, dashboard page, drag-and-drop, back-end interactions, modal windows, video-guide.',
     },
     image: 'YG.jpg',
     link: 'https://github.com/YaGrig',
